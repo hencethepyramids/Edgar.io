f30dfd8553fd33cef298de0c2dcd17bd507b5c20
Ethan Ellerstein <eellerstein.gmail.com>
2023-12-16 17:14:05 -0600

    updated cursor

--- game.js
+++ game.js
@@ -6,6 +6,8 @@ const ctx = canvas.getContext('2d');
 const colorPicker = document.getElementById('colorPicker');
 const fileInput = document.getElementById('fileInput');
 const fileInputLabel = document.getElementById('fileInputLabel');
+const playBtn = document.getElementById('playBtn');
+const customizeBallBtn = document.getElementById('customizeBallBtn');
 
 // Set canvas dimensions based on window size
 canvas.width = window.innerWidth;
@@ -15,15 +17,19 @@ let player;
 const foods = [];
 const mapSpeedMultiplier = 0.02;
 
+let selectedColor = '#ff00ff'; // Default color
+
 function startGame() {
+  // Hide color picker and file input
+  colorPicker.style.display = 'none';
+  fileInput.style.display = 'none';
+  fileInputLabel.style.display = 'none';
+
+  // Hide Play button
+  playBtn.style.display = 'none';
+
   startScreen.style.display = 'none';
   gameScreen.style.display = 'block';
-
-  // Get color from the color picker
-  const selectedColor = colorPicker.value;
-
-  // Enable the color picker option
-  player = createPlayer(event.clientX, event.clientY, 10, selectedColor);
 
   // Enable the lines below to enable the image upload option
   fileInputLabel.style.display = 'block';
@@ -40,7 +46,7 @@ function startGame() {
   canvas.style.opacity = '0.5'; // Adjust the opacity as needed
 
   // Set the initial size of the player
-  player.radius = player.initialRadius;
+  player = createPlayer(canvas.width / 2, canvas.height / 2, 10, selectedColor);
 
   // Resize canvas on window resize
   window.addEventListener('resize', function () {
@@ -96,38 +102,45 @@ function drawFood(food) {
 }
 
 function update(event) {
-  const mouseX = event.clientX;
-  const mouseY = event.clientY;
+  if (player) {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
 
-  // Apply physics to player movement
-  const dx = mouseX - player.x;
-  const dy = mouseY - player.y;
-  const angle = Math.atan2(dy, dx);
-  const force = 0.1;
+    // Normalize the mouse coordinates based on canvas dimensions
+    const canvasRect = canvas.getBoundingClientRect();
+    const normalizedMouseX = (mouseX - canvasRect.left) / canvasRect.width;
+    const normalizedMouseY = (mouseY - canvasRect.top) / canvasRect.height;
 
-  player.speed += force;
-  player.x += player.speed * Math.cos(angle);
-  player.y += player.speed * Math.sin(angle);
+    // Calculate the angle between the current position and the normalized mouse position
+    const dx = normalizedMouseX * canvas.width - player.x;
+    const dy = normalizedMouseY * canvas.height - player.y;
+    const angle = Math.atan2(dy, dx);
 
-  // Apply friction to slow down player
-  player.speed *= player.friction;
+    // Set the speed and direction of the player towards the normalized mouse position
+    player.speed = 5; // You can adjust the speed as needed
+    player.x += player.speed * Math.cos(angle);
+    player.y += player.speed * Math.sin(angle);
 
-  // Check for collision with food
-  for (let i = foods.length - 1; i >= 0; i--) {
-    const food = foods[i];
-    const distance = Math.sqrt((player.x - food.x) ** 2 + (player.y - food.y) ** 2);
+    // Apply friction to slow down player
+    player.speed *= player.friction;
 
-    if (distance < player.radius + food.radius) {
-      // Player consumed the food
-      foods.splice(i, 1);
-      player.radius += 2;
-      player.speed -= 0.02;
+    // Check for collision with food
+    for (let i = foods.length - 1; i >= 0; i--) {
+      const food = foods[i];
+      const distance = Math.sqrt((player.x - food.x) ** 2 + (player.y - food.y) ** 2);
+
+      if (distance < player.radius + food.radius) {
+        // Player consumed the food
+        foods.splice(i, 1);
+        player.radius += 2;
+        player.speed -= 0.02;
+      }
     }
-  }
 
-  // Move the map with the player
-  canvas.style.backgroundPositionX = -player.x * mapSpeedMultiplier + 'px';
-  canvas.style.backgroundPositionY = -player.y * mapSpeedMultiplier + 'px';
+    // Move the map with the player
+    canvas.style.backgroundPositionX = -player.x * mapSpeedMultiplier + 'px';
+    canvas.style.backgroundPositionY = -player.y * mapSpeedMultiplier + 'px';
+  }
 }
 
 function draw() {
@@ -150,8 +163,8 @@ function gameloop() {
 }
 
 colorPicker.addEventListener('input', function () {
-  // Enable the color picker option
-  player.color = colorPicker.value;
+  // Update the selected color
+  selectedColor = colorPicker.value;
 });
 
 fileInput.addEventListener('change', function () {
@@ -166,26 +179,48 @@ fileInput.addEventListener('change', function () {
   }
 });
 
-function customizeBall() {
-  const colorPicker = document.getElementById('colorPicker');
-  const fileInput = document.getElementById('fileInput');
-  const fileInputLabel = document.getElementById('fileInputLabel');
-
-  // Show color picker and hide file input initially
+// Updated openColorPicker function
+function openColorPicker() {
+  // Show color picker and hide file input
   colorPicker.style.display = 'block';
   fileInput.style.display = 'none';
   fileInputLabel.style.display = 'none';
 
-  // Change file input visibility when needed
-  colorPicker.addEventListener('input', function () {
-    colorPicker.style.display = 'block';
-    fileInput.style.display = 'none';
-    fileInputLabel.style.display = 'none';
-  });
+  // Hide Play button
+  playBtn.style.display = 'none';
 
-  fileInput.addEventListener('change', function () {
-    colorPicker.style.display = 'none';
-    fileInput.style.display = 'block';
-    fileInputLabel.style.display = 'block';
-  });
+  // Add event listener for color selection
+  colorPicker.addEventListener('input', handleColorSelection);
 }
+
+// Event handler for color selection
+function handleColorSelection() {
+  // Update the selected color
+  selectedColor = colorPicker.value;
+
+  // Show Play button
+  playBtn.style.display = 'block';
+}
+
+// Event handler for file input (image upload)
+function handleImageUpload() {
+  const file = fileInput.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      // Handle image upload logic
+      // For now, just log the result to the console
+      console.log(e.target.result);
+
+      // Show Play button
+      playBtn.style.display = 'block';
+    };
+    reader.readAsDataURL(file);
+  }
+}
+
+// Update the Customize Ball button to call the new function
+customizeBallBtn.addEventListener('click', openColorPicker);
+
+// Update the Play button to call the new function
+playBtn.addEventListener('click', startGame);
